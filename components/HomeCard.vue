4ae75265ab38f42d28c96725cd81af1c2766cb42
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-10 16:28:33 +0330

    Incomplete App

---
+++ components/HomeCard.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <img :src="home.images[0]" style="width:200px"> <br/>
+       {{home.title}}<br/>
+       {{ home.loaction.city }}{{ home.loaction }} {{ home.loaction }}<br/>
+       {{home.guests}}guests<br/>
+       {{home.pricePerNight}} /night <br/>
+</div>
+  
+       
+</template>
+<script>
+export default {
+    props:{
+        home:{
+            type:Object,
+            required:true
+        }
+    }
+}
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+       <div v-for="home in homes" :key="home.objectID" style="float:Left;margin:10px">
+        <home-card-vue :home="home"></home-card-vue>
+       </div>
+    </div>
+</template>
+<script>
+import homes from '../data/homes.json'
+//import HomeCard from '../components/HomeCard.vue'
+export default {
+   // components:{
+      //  HomeCard,
+  //  },
+  head(){
+      return {
+          title:'Homepage',
+          meta:[{
+              name:'description',
+              content:'This is The Homepage',
+              hid:'description'
+          }]
+      }
+  },
+    data(){
+        return {
+            homes:homes.slice(0,3)
+        }
+    }
+}
+</script>
